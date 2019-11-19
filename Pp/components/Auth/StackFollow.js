014a73ae6084baadb6760ef577fc4f6fcb00d2ce
Bryan Valerio <bryanvale91@gmail.com>
2019-11-18 18:01:15 -0600

    coding the functions for each component

---
+++ Pp/components/Auth/StackFollow.js
@@ -0,0 +1,21 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { TabFollow } from './TabFollow';
+import { Profile, Publication, Comments } from '.';
+
+const StackFollow = createStackNavigator({
+    TabFollow: {
+        screen: TabFollow,
+    },
+    Author: {
+        screen: Profile,
+    },
+    Publication: {
+        screen: Publication,
+    },
+    Comments: {
+        screen: Comments,
+    },
+
+});
+
+export { StackFollow };
---
+++ Pp/components/Auth/index.js
@@ -0,0 +1,17 @@
+import Add from './Add';
+import Follow from './Follow';
+import Home from './Home';
+import Profile from './Profile';
+import Search from './Search';
+import Publication from './Publication';
+import Comments from './Comments';
+
+export {
+    Add,
+    Follow,
+    Home,
+    Profile,
+    Search,
+    Publication,
+    Comments,
+};
